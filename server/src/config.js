a0dbe67b058a46158dc20e7f9ebf094e55680c76
VikasShetty <shettyvikas806@gmail.com>
2026-08-27 20:01:57 +0530

    error update

--- server/src/config.js
+++ server/src/config.js
@@ -3,16 +3,25 @@ import "dotenv/config";
 export const config = {
     port: Number(process.env.PORT || 3000),
 
-    host: process.env.HOST || "0.0.0.0",
+    host:
+        process.env.HOST || "0.0.0.0",
 
-    nodeEnv: process.env.NODE_ENV || "production",
+    nodeEnv:
+        process.env.NODE_ENV || "development",
 
     clientUrl:
         process.env.CLIENT_URL ||
         "https://temp-file-drop.pages.dev",
 
-    b2KeyId: process.env.B2_KEY_ID,
-    b2ApplicationKey: process.env.B2_APPLICATION_KEY,
-    b2BucketName: process.env.B2_BUCKET_NAME,
-    b2Endpoint: process.env.B2_ENDPOINT
+    b2KeyId:
+        process.env.B2_KEY_ID,
+
+    b2ApplicationKey:
+        process.env.B2_APPLICATION_KEY,
+
+    b2BucketName:
+        process.env.B2_BUCKET_NAME,
+
+    b2Endpoint:
+        process.env.B2_ENDPOINT
 };
